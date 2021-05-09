6fc966d58fa044296fe5b3da8ec77cafe3f60abd
Sumija Ande <andesumija@gmail.com>
2021-05-09 14:00:24 -0700

    fixed up map

--- src/components/ActivityCard.vue
+++ src/components/ActivityCard.vue
@@ -6,8 +6,8 @@
     ></v-img>
 
     <v-card-title> {{ activityName }} </v-card-title>
-    <v-card-subtitle> {{ cost }} points </v-card-subtitle>
-    <v-card-subtitle> {{ categories }} </v-card-subtitle>
+    <v-card-subtitle> Cost:{{ cost }} points </v-card-subtitle>
+    <v-card-subtitle> Categories: {{ categories }} </v-card-subtitle>
     <v-card-actions v-if="isActivityCard">
       <v-btn color="orange lighten-2" text @click="show = !show">
         Details
@@ -30,12 +30,8 @@
     </v-card-actions>
 
     <v-card-actions v-if="isAdminCard">
-      <v-btn color="green lighten-2" text>
-        Approve
-      </v-btn>
-      <v-btn color="red lighten-2" text>
-        Deny
-      </v-btn>
+      <v-btn color="green lighten-2" text @click="approve()"> Approve </v-btn>
+      <v-btn color="red lighten-2" text @click="deny()"> Deny </v-btn>
       <v-spacer></v-spacer>
     </v-card-actions>
 
@@ -43,10 +39,10 @@
       <div v-show="show">
         <v-divider></v-divider>
         <v-card-text> {{ description }} </v-card-text>
-        
-          <router-link :to="{ name: 'MapView', params: { destination: address, propPlace: origin } }">
-            <v-btn class="ma-5">Location</v-btn>
-          </router-link>
+
+        <router-link :to="{ name: 'MapView', params: { destination: location, propPlace: origin } }">
+          <v-btn class="ma-5">Location</v-btn>
+        </router-link>
         <!-- </v-btn> -->
       </div>
     </v-expand-transition>
@@ -56,29 +52,19 @@
 
 
 <script>
-
-import firebase from 'firebase';
-//import 'firebase/firestore'
-//import firebaseApp from '../main';
-
-//var citizens = firebase.firestore().collection("/Citizens");
-
+import firebase from "firebase";
 export default {
   name: "ActivityCard",
   data() {
     return {
       show: false,
-      user: this.$store.state.user.email,
-      userType: this.$store.state.user.userType,
-      name: this.$store.state.user.name,
-      points: this.$store.state.user.points,
-      docID: this.$store.state.user.docID,
-      //set address to actual event address 
+      //set address to actual event address
       location: "200 S Mathilda Ave, Sunnyvale, CA",
+      origin: null,
     };
   },
   mounted() {
-    console.log({ router: this.$router });
+    // console.log({ router: this.$router });
     this.getLocation();
   },
   props: {
@@ -93,35 +79,68 @@ export default {
     isProfileCard: Boolean,
     isVenderCard: Boolean,
     isAdminCard: Boolean,
-    address: String
+    address: String,
+    id: String,
   },
   methods: {
-    purchase: async function () {
-        //alert("aaaa")
-        //alert(this.docID);
-        const db = firebase.firestore();
-        const data = await db.collection("Activities").where("name", "==", this.activityName).get();
-        const activityDocID = data.docs[0].id;
-        const activityRef = "/Activities/" + activityDocID;
-        //alert(activityDocID)
-        console.log("here1 ")
-        if(this.points>=this.cost){
-          console.log("here 2 ");
-          console.log(activityRef);
-          console.log(this.docID);
-          var adding = {
-            "activity": db.doc('/Activities/' + activityDocID),
-            "datePurchased": Date.now()
-          };
-          db.collection("Citizens").doc(this.docID).update({
-            "points": this.points - this.cost,
-            "purchased": firebase.firestore.FieldValue.arrayUnion(adding)
-          });
-          this.$store.state.user.points = this.points - this.cost;
-          alert("check the database");
-        } else {
-          alert("You currently do not have sufficient points to purchase this activity. Please purchase more points first")
-        }
+    leave: function () {
+      this.$router.replace("/login");
+      alert(`Redirecting to login page`);
+    },
+    purchase: function () {
+      let currentUser;
+      firebase.auth().onAuthStateChanged(function (user) {
+        currentUser = user;
+      });
+      if (currentUser) {
+        // User is signed in.
+      } else {
+        // No user is signed in.
+        this.$router.replace("/login");
+        alert(`Redirecting to login page`);
+      }
+    },
+    approve: async function () {
+      console.log(this.$props.id);
+      const db = firebase.firestore();
+
+      await db
+        .collection("Activities")
+        .doc(this.$props.id)
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            doc.ref.update({
+              approved: true,
+              adminApproved: true,
+            });
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting documents: ", error);
+        });
+    },
+    deny: async function () {
+      const db = firebase.firestore();
+      await db
+        .collection("Activities")
+        .doc(this.$props.id)
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            doc.ref.update({
+              approved: true,
+              adminApproved: false,
+            });
+          } else {
+            console.log("No such document!");
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting documents: ", error);
+        });
     },
     getLocation: function () {
       if(!("geolocation" in navigator)) {
@@ -130,14 +149,18 @@ export default {
       else
       {
         navigator.geolocation.getCurrentPosition(pos => {
-          console.log("position: ", pos.coords)
+          // console.log("position: ", pos.coords)
           this.origin = pos.coords;
         }, err => {
           console.log("error: ", err.message);
         })
       }
+      if(this.address != null)
+      {
+        this.location = this.address;
+      }
     },
-  }
+  },
 };
 </script>
 
--- src/components/MapView.vue
+++ src/components/MapView.vue
@@ -33,7 +33,7 @@ export default {
   },
   methods: {
     initializeHereMap() { // rendering map
-      console.log("propPlace: ", this.propPlace);
+      // console.log("propPlace: ", this.propPlace);
       this.origin = this.propPlace.latitude + ',' + this.propPlace.longitude;
 
       const mapContainer = this.$refs.hereMap;
@@ -60,7 +60,7 @@ export default {
         }, (result) => {
           // Add a marker for each location found
           result.items.forEach((item) => {
-            console.log("The destination is:\n", item.position.lat + ',' + item.position.lng);
+            // console.log("The destination is:\n", item.position.lat + ',' + item.position.lng);
             this.location = item.position.lat + ',' + item.position.lng;
 
             //Find and plot route
@@ -105,7 +105,7 @@ export default {
 
             // Get an instance of the routing service version 8:
             var router = this.platform.getRoutingService(null, 8);
-            console.log("zoom: ", map.getCenter())
+            // console.log("center: ", map.getCenter())
             let whatever = {lat: ((item.position.lat + this.propPlace.latitude)/2), lng: ((item.position.lng + this.propPlace.longitude)/2)}
             map.setCenter(whatever);
             // console.log("zoom: ", map.getZoom())
