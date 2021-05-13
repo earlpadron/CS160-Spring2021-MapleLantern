bed12c1e792bb613885bf61f1b9327dbe2fad0d8
xinru <xinrulin99@gmail.com>
2021-05-12 21:06:36 -0700

    created contact.vue

---
+++ src/views/Contact.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class='contact'>
+    <v-container></v-container>
+    <v-container>
+      <v-form>
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          label="Name"
+          required
+        ></v-text-field>
+
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          required
+        ></v-text-field>
+        <v-text-field label="Message" v-model="message"></v-text-field>
+        <v-btn :disabled="!valid" color="success" class="mr-4" @click="send">
+          Submit
+        </v-btn>
+      </v-form>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+export default {
+  name: "Contact",
+  data() {
+    return {
+      valid: true,
+      name: "",
+      nameRules: [
+        (v) => !!v || "Name is required",
+        (v) =>
+          (v && v.length <= 100) || "Name must be less than 100 characters",
+      ],
+      email: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
+      ],
+      message: "",
+    };
+  },
+  methods: {
+    send: function () {
+      const db = firebase.firestore();
+      db.collection("Messages")
+        .add({
+          name: this.name,
+          email: this.email,
+          message: this.message,
+          reviewed: false,
+        })
+        .then(() => {
+          alert("Successfully submited message");
+        })
+        .catch((err) => {
+          console.error("Error has occurred when added the data: ", err);
+        });
+    },
+  },
+};
+</script>
+<style scoped>
+.contact{
+    padding-top: 50px;
+}
+</style>
